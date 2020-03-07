b38f5a5bb9a550dab098223e0b9018154414a39b
Paul Taiwo <ayopaulot@gmail.com>
2020-03-07 01:56:59 +0100

    add a default route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import BillingComponent from "./components/Billing";
 
 const App = () => (
     <BrowserRouter>
         <Switch>
+            <Redirect exact from='/' to='/billings' />
             <Route path='/billings' component={BillingComponent} />
         </Switch>
     </BrowserRouter>
